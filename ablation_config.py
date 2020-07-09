8e478063f8f1bae4e35ed247034d5e3830406353
Varun Sundar <varun19299@gmail.com>
2020-07-09 15:52:19 +0530

    Add: ablative configs, added LPIPS to val

--- ablation_config.py
+++ ablation_config.py
@@ -802,6 +802,234 @@ def ours_toled_ECA():
 
 
 # Loss functions
+def ours_poled_l1_ms_ssim():
+    exp_name = "ours-poled-512-l1-ms-ssim"
+
+    batch_size = 4
+    do_augment = True
+    num_epochs = 448
+
+    # Model args
+    model = "atrous-guided-filter-pixelshuffle-gca-atrous-corrected"
+    pixelshuffle_ratio = 2
+    guided_map_kernel_size = 3
+    guided_map_channels = 16
+
+    num_threads = batch_size * 2
+    log_interval = 25
+    val_test_epoch_interval = 6
+    save_copy_every_epochs = 64
+
+    # Loss
+    lambda_image = 1 - 0.84
+    lambda_ms_ssim = 0.84
+
+    image_height = 512
+    image_width = 1024
+
+    # Cosine annealing
+    T_0 = 64
+    T_mult = 2
+
+    learning_rate = 3e-4
+
+
+def ours_poled_l1_percep():
+    exp_name = "ours-poled-512-l1-percep"
+
+    batch_size = 4
+    do_augment = True
+    num_epochs = 448
+
+    # Model args
+    model = "atrous-guided-filter-pixelshuffle-gca-atrous-corrected"
+    pixelshuffle_ratio = 2
+    guided_map_kernel_size = 3
+    guided_map_channels = 16
+
+    num_threads = batch_size * 2
+    log_interval = 25
+    val_test_epoch_interval = 6
+    save_copy_every_epochs = 64
+
+    # Loss
+    lambda_perception = 1.2
+
+    image_height = 512
+    image_width = 1024
+
+    # Cosine annealing
+    T_0 = 64
+    T_mult = 2
+
+    learning_rate = 3e-4
+
+
+def ours_poled_l1_percep_adv():
+    exp_name = "ours-poled-512-l1-percep-adv"
+
+    batch_size = 4
+    do_augment = True
+    num_epochs = 448
+
+    # Model args
+    model = "atrous-guided-filter-pixelshuffle-gca-atrous-corrected"
+    pixelshuffle_ratio = 2
+    guided_map_kernel_size = 3
+    guided_map_channels = 16
+
+    num_threads = batch_size * 2
+    log_interval = 25
+    val_test_epoch_interval = 6
+    save_copy_every_epochs = 64
+
+    # Loss
+    lambda_perception = 1.2
+    lambda_adversarial = 0.6
+
+    image_height = 512
+    image_width = 1024
+
+    # Cosine annealing
+    T_0 = 64
+    T_mult = 2
+
+    learning_rate = 3e-4
+
+
+def ours_toled_l1_ms_ssim():
+    exp_name = "ours-toled-512-l1-ms-ssim"
+
+    batch_size = 4
+    do_augment = True
+    num_epochs = 448
+
+    # Model args
+    model = "atrous-guided-filter-pixelshuffle-gca-atrous-corrected"
+    pixelshuffle_ratio = 2
+    guided_map_kernel_size = 3
+    guided_map_channels = 16
+
+    num_threads = batch_size * 2
+    log_interval = 25
+    val_test_epoch_interval = 6
+    save_copy_every_epochs = 64
+
+    image_height = 512
+    image_width = 1024
+
+    # Loss
+    lambda_image = 1 - 0.84
+    lambda_ms_ssim = 0.84
+
+    # ---------------------------------------------------------------------------- #
+    # Data
+    # ---------------------------------------------------------------------------- #
+
+    image_dir = Path("data")
+    train_source_dir = image_dir / "Toled_train" / "LQ"
+    train_target_dir = image_dir / "Toled_train" / "HQ"
+
+    val_source_dir = None
+    val_target_dir = None
+
+    test_source_dir = image_dir / "Toled_val" / "LQ"
+
+    # Cosine annealing
+    T_0 = 64
+    T_mult = 2
+
+    learning_rate = 3e-4
+
+
+def ours_toled_l1_percep():
+    exp_name = "ours-toled-512-l1-percep"
+
+    batch_size = 4
+    do_augment = True
+    num_epochs = 448
+
+    # Model args
+    model = "atrous-guided-filter-pixelshuffle-gca-atrous-corrected"
+    pixelshuffle_ratio = 2
+    guided_map_kernel_size = 3
+    guided_map_channels = 16
+
+    num_threads = batch_size * 2
+    log_interval = 25
+    val_test_epoch_interval = 6
+    save_copy_every_epochs = 64
+
+    image_height = 512
+    image_width = 1024
+
+    # Loss
+    lambda_perception = 1.2
+
+    # ---------------------------------------------------------------------------- #
+    # Data
+    # ---------------------------------------------------------------------------- #
+
+    image_dir = Path("data")
+    train_source_dir = image_dir / "Toled_train" / "LQ"
+    train_target_dir = image_dir / "Toled_train" / "HQ"
+
+    val_source_dir = None
+    val_target_dir = None
+
+    test_source_dir = image_dir / "Toled_val" / "LQ"
+
+    # Cosine annealing
+    T_0 = 64
+    T_mult = 2
+
+    learning_rate = 3e-4
+
+
+def ours_toled_l1_percep_adv():
+    exp_name = "ours-toled-512-l1-percep-adv"
+
+    batch_size = 4
+    do_augment = True
+    num_epochs = 448
+
+    # Model args
+    model = "atrous-guided-filter-pixelshuffle-gca-atrous-corrected"
+    pixelshuffle_ratio = 2
+    guided_map_kernel_size = 3
+    guided_map_channels = 16
+
+    num_threads = batch_size * 2
+    log_interval = 25
+    val_test_epoch_interval = 6
+    save_copy_every_epochs = 64
+
+    image_height = 512
+    image_width = 1024
+
+    # Loss
+    lambda_perception = 1.2
+    lambda_adversarial = 0.6
+
+    # ---------------------------------------------------------------------------- #
+    # Data
+    # ---------------------------------------------------------------------------- #
+
+    image_dir = Path("data")
+    train_source_dir = image_dir / "Toled_train" / "LQ"
+    train_target_dir = image_dir / "Toled_train" / "HQ"
+
+    val_source_dir = None
+    val_target_dir = None
+
+    test_source_dir = image_dir / "Toled_val" / "LQ"
+
+    # Cosine annealing
+    T_0 = 64
+    T_mult = 2
+
+    learning_rate = 3e-4
+
 
 ablative_configs = [
     ours_poled,
@@ -831,4 +1059,10 @@ ablative_configs = [
     ours_toled_no_CA,
     ours_toled_ECA,
     # loss functions
+    ours_poled_l1_ms_ssim,
+    ours_poled_l1_percep,
+    ours_poled_l1_percep_adv,
+    ours_toled_l1_ms_ssim,
+    ours_toled_l1_percep,
+    ours_toled_l1_percep_adv,
 ]
